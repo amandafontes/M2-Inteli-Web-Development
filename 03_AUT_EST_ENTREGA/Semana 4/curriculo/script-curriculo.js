eaea6f50ef47bed0e2a866b5dd81debe7bcef158
Amanda <amanda.fontes@sou.inteli.edu.br>
2022-05-13 23:59:32 -0300

    Exercício de personalização da Etapa 4
    Currículo com Javascript.

--- 03_AUT_EST_ENTREGA/Semana 4/curriculo/script-curriculo.js
+++ 03_AUT_EST_ENTREGA/Semana 4/curriculo/script-curriculo.js
@@ -9,27 +9,3 @@ function Text(value){
         document.getElementById("box1").style.display = 'none';
     }
 }
-
-// window.alert("Você está acessando um currículo.");
-
-// var geral = document.getElementById("botao");
-// var box1 = document.getElementById("box1");
-
-
-// function Inicio() {
-//     geral.removeChild(box2);
-//     geral.removeChild(box3);
-// }
-
-// function Text() {       
-//     geral.appendChild(box1);
-//     // box3.innerHTML = "<a> ps: Este texto foi gerado com javascript e o botão também :) </a>";
-//     button.innerHTML = "<button id='button' onclick='Voltar()'>DESTAQUES ACADÊMICOS</button>";
-// }   
-
-// function Voltar() {
-//     geral.appendChild(box1);
-//     geral.removeChild(box1);    
-//     // geral.removeChild(box3);
-//     button.innerHTML = "<button id='button' onclick='Text()'>DESTAQUES ACADÊMICOS</button>"
-// }
