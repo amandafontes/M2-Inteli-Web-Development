92a84648cc94f20379035178991cd7bcca91cc53
Amanda <amanda.fontes@sou.inteli.edu.br>
2022-05-23 14:38:22 -0300

    Resolução de problemas com algoritmos - semana 5

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/Exercícios/atividade1.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/Exercícios/atividade1.js
@@ -1,7 +1,37 @@
-var num1 = Number;
-var num2 = Number;
+function calculate() {
 
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var operação = document.getElementById("operação").value;
+    var resultado;
 
-function getElementById(){
-    
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+
+    if (operação == "soma") {
+        resultado = num1 + num2;
+    }
+
+    if (operação == "subtração") {
+        resultado = num1 - num2;
+    }
+
+    if (operação == "multiplicação") {
+        resultado = num1 * num2;
+    }
+
+    if (operação == "divisãoInteira") {
+        resultado = Math.floor(num1 / num2);
+    }
+
+    if (operação == "divisãoExata") {
+        resultado = num1 / num2;
+    }
+
+    if (operação == "módulo") {
+        resultado = num1 % num2;
+    }
+
+    // document.getElementById("resultado") = resultado;
+    document.getElementById("resultado").innerText = resultado;
 }
