125d652e32aef724aaf13b4573c090b67d08573f
Amanda <amanda.fontes@sou.inteli.edu.br>
2022-06-20 22:08:59 -0300

    Revert "Exercícios Obrigatórios - Semana 9"
    This reverts commit 45b163b52c2f43d2e3c26fd495ec47492d45d04f.

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/ex2.js
+++
@@ -1,5 +0,0 @@
-function somar() {
-    var numero = document.getElementById('numero').value;
-    resultado = parseInt(numero[0]) + parseInt(numero[1]) + parseInt(numero[2]);
-    document.getElementById("soma").innerText = "Soma dos dígitos: " + resultado;
-}
